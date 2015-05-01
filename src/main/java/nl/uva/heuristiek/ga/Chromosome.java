ca6815612f7defc78757cdc703dbf640e6c958c0
Remco Mokveld <remco@thecapitals.nl>
2015-05-01 14:47:34 +0200

    missed ga class

---
+++ src/main/java/nl/uva/heuristiek/ga/Chromosome.java
@@ -0,0 +1,30 @@
+package nl.uva.heuristiek.ga;
+
+import nl.uva.heuristiek.model.Penalty;
+import nl.uva.heuristiek.model.Schedule;
+
+/**
+ * Created by remco on 22/04/15.
+ */
+public class Chromosome {
+    private int[] mActivitySlots;
+    private Penalty mPenalty;
+
+    public Chromosome(Schedule schedule) {
+        mActivitySlots = schedule.getActivitySlots();
+        mPenalty = schedule.getPenalty(false);
+    }
+
+    public Chromosome(int[] activitySlots, Penalty penalty) {
+        mActivitySlots = activitySlots;
+        mPenalty = penalty;
+    }
+
+    public int[] getActivitySlots() {
+        return mActivitySlots;
+    }
+
+    public Penalty getPenalty() {
+        return mPenalty;
+    }
+}
